d73fe868b8cc7c06eb7a463869d03efd86f71632
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-04-04 13:21:56 -0700

    Stage up project

---
+++ GreatSwords/ModCore.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using BepInEx;
+using BepInEx.Configuration;
+using ItemManager;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GreatSwords
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class GreatSwordsMod : BaseUnityPlugin
+    {
+        private const string ModName = "GreatSwords";
+        internal const string ModVersion = "0.0.1";
+        private const string ModGUID = "com.littleroomdev.greatswords";
+        private static Harmony harmony = null!;
+        internal static Item GreatSwordSilver;
+        internal static Item GreatSwordIron;
+        internal static Item GreatSwordBronze;
+        internal static Item GreatSwordBlackMetal;
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            harmony = new(ModGUID);
+            harmony.PatchAll(assembly);
+
+            GreatSwordSilver = new Item("greatswords", "Sword_GreatSilver");
+            GreatSwordIron = new Item("greatswords", "Sword_GreatIron");
+            GreatSwordBronze = new Item("greatswords", "Sword_GreatBronze");
+            GreatSwordBlackMetal = new Item("greatswords", "Sword_GreatBlackmetal");
+
+           
+            
+            GreatSwordBronze.Name.English("Bronze 2H Sword");
+            GreatSwordBronze.Description.English("It's big...");
+            GreatSwordBronze.Crafting.Add(CraftingTable.Forge, 0);
+            GreatSwordBronze.RequiredItems.Add("Bronze",15);
+            GreatSwordBronze.RequiredItems.Add("Wood",2);
+            GreatSwordBronze.RequiredItems.Add("LeatherScraps",4);
+
+            GreatSwordBronze.Configurable = Configurability.Recipe;
+
+            GreatSwordIron.Name.English("Iron 2H Sword");
+            GreatSwordIron.Description.English("Cleave your enemies in twain.");
+            GreatSwordIron.Crafting.Add(CraftingTable.Forge, 1);
+            GreatSwordIron.RequiredItems.Add("Iron",30);
+            GreatSwordIron.RequiredItems.Add("Wood",2);
+            GreatSwordIron.RequiredItems.Add("LeatherScraps",4);
+
+            GreatSwordIron.Configurable = Configurability.Recipe;
+
+
+            GreatSwordSilver.Name.English("Silver 2H Sword");
+            GreatSwordSilver.Description.English("Pure shine of silver wraps this marvellous blade.");
+            GreatSwordSilver.Crafting.Add(CraftingTable.Forge, 2);
+            GreatSwordSilver.RequiredItems.Add("Silver",30);
+            GreatSwordSilver.RequiredItems.Add("Iron",10);
+            GreatSwordSilver.RequiredItems.Add("Wood",2);
+            GreatSwordSilver.RequiredItems.Add("LeatherScraps",4);
+
+            GreatSwordSilver.Configurable = Configurability.Recipe;
+
+
+            GreatSwordBlackMetal.Name.English("BlackMetal 2H Sword");
+            GreatSwordBlackMetal.Description.English("They will bow to it.");
+            GreatSwordBlackMetal.Crafting.Add(CraftingTable.BlackForge, 0);
+            GreatSwordBlackMetal.RequiredItems.Add("BlackMetal",40);
+            GreatSwordBlackMetal.RequiredItems.Add("FineWood",2);
+            GreatSwordBlackMetal.RequiredItems.Add("JuteRed",4);
+
+            GreatSwordBlackMetal.Configurable = Configurability.Recipe;
+
+        }
+    }
+}
---
+++ GreatSwords/Patches.cs
@@ -0,0 +1,84 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace GreatSwords
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(ZoneSystem), nameof(ZoneSystem.Awake))]
+        public static class ObjectAssignerPatch
+        {
+            public static void Postfix()
+            {
+                ItemDrop donorSword = null;
+                foreach (var VARIABLE in Resources.FindObjectsOfTypeAll<GameObject>())
+                {
+                    if (VARIABLE.name == "THSwordKrom")
+                    {
+                        donorSword = VARIABLE.gameObject.GetComponent<ItemDrop>();
+                    }
+                }
+                if (!donorSword)
+                {
+                    Debug.LogError("Donor sword cant be found for SFX");
+                    return;
+                }
+                var hitEffects = new EffectList();
+                var hitTerrainEffects = new EffectList();
+                var blockEffects = new EffectList();
+                var triggerEffects = new EffectList();
+                var trailEffects = new EffectList();
+            
+                hitEffects = donorSword.m_itemData.m_shared.m_hitEffect;
+                hitTerrainEffects = donorSword.m_itemData.m_shared.m_hitTerrainEffect;
+                blockEffects = donorSword.m_itemData.m_shared.m_blockEffect;
+                triggerEffects = donorSword.m_itemData.m_shared.m_triggerEffect;
+                trailEffects = donorSword.m_itemData.m_shared.m_trailStartEffect;
+
+                GreatSwordsMod.GreatSwordSilver.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_hitEffect = hitEffects;
+                GreatSwordsMod.GreatSwordSilver.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_hitTerrainEffect = hitTerrainEffects;
+                GreatSwordsMod.GreatSwordSilver.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_blockEffect = blockEffects;
+                GreatSwordsMod.GreatSwordSilver.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_triggerEffect = triggerEffects;
+                GreatSwordsMod.GreatSwordSilver.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_trailStartEffect = trailEffects;
+
+                GreatSwordsMod.GreatSwordIron.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_hitEffect = hitEffects;
+                GreatSwordsMod.GreatSwordIron.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_hitTerrainEffect = hitTerrainEffects;
+                GreatSwordsMod.GreatSwordIron.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_blockEffect = blockEffects;
+                GreatSwordsMod.GreatSwordIron.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_triggerEffect = triggerEffects;
+                GreatSwordsMod.GreatSwordIron.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_trailStartEffect = trailEffects;
+
+                GreatSwordsMod.GreatSwordBronze.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_hitEffect = hitEffects;
+                GreatSwordsMod.GreatSwordBronze.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_hitTerrainEffect = hitTerrainEffects;
+                GreatSwordsMod.GreatSwordBronze.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_blockEffect = blockEffects;
+                GreatSwordsMod.GreatSwordBronze.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_triggerEffect = triggerEffects;
+                GreatSwordsMod.GreatSwordBronze.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_trailStartEffect = trailEffects;
+
+                GreatSwordsMod.GreatSwordBlackMetal.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_hitEffect = hitEffects;
+                GreatSwordsMod.GreatSwordBlackMetal.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_hitTerrainEffect = hitTerrainEffects;
+                GreatSwordsMod.GreatSwordBlackMetal.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_blockEffect = blockEffects;
+                GreatSwordsMod.GreatSwordBlackMetal.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_triggerEffect = triggerEffects;
+                GreatSwordsMod.GreatSwordBlackMetal.Prefab.gameObject.GetComponent<ItemDrop>().m_itemData.m_shared
+                    .m_trailStartEffect = trailEffects;
+            }
+        }
+    }
+}
